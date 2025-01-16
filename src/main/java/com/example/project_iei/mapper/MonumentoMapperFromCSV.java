3dd445bf352866d310fab79dab1d4d166498b04a
vic0rv <victor30072002@hotmail.com>
2025-01-16 14:30:51 +0100

    Selenium

--- src/main/java/com/example/project_iei/mapper/MonumentoMapperFromCSV.java
+++ src/main/java/com/example/project_iei/mapper/MonumentoMapperFromCSV.java
@@ -7,9 +7,17 @@ import com.example.project_iei.entity.Provincia;
 import com.example.project_iei.entity.TipoMonumento;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.checkerframework.checker.units.qual.C;
 import org.locationtech.proj4j.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -97,26 +105,49 @@ public class MonumentoMapperFromCSV {
     }
 
 
+
     public static List<String> convertUTMToLatLng(double utmE, double utmN, String utmZone) {
-        // Crear sistemas de referencia
-        CRSFactory crsFactory = new CRSFactory();
+
+        List<String> result = new ArrayList<>();
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ign.es/web/calculadora-geodesica");
+
+        WebElement radioButton = new WebDriverWait(driver, Duration.ofSeconds(3))
+                .until(ExpectedConditions.elementToBeClickable(By.id("utm")));
+        radioButton.click();
+
+        WebElement inputX = driver.findElement(By.id("datacoord1"));
+
+        inputX.sendKeys(String.valueOf(utmN));
+
+        WebElement inputY = driver.findElement(By.id("datacoord2"));
+
+        inputY.sendKeys(String.valueOf(utmE));
+
+        driver.findElement(By.id("trd_calc")).click();
+
+        WebDriverWait webDriverWait1 = new WebDriverWait(driver, Duration.ofSeconds(2));
+
+        WebElement longitud = driver.findElement(By.id("txt_etrs89_longd"));
+
+        WebElement latitud = driver.findElement(By.id("txt_etrs89_latgd"));
+
+        /*CRSFactory crsFactory = new CRSFactory();
         CoordinateReferenceSystem utmCrs = crsFactory.createFromName("EPSG:326" + utmZone); // EPSG para UTM Norte
         CoordinateReferenceSystem wgs84 = crsFactory.createFromName("EPSG:4326"); // WGS84 Lat/Lng
 
-        // Configurar la transformación
         CoordinateTransformFactory transformFactory = new CoordinateTransformFactory();
         CoordinateTransform transform = transformFactory.createTransform(utmCrs, wgs84);
 
-        // Transformar coordenadas
         ProjCoordinate source = new ProjCoordinate(utmN, utmE);
         ProjCoordinate target = new ProjCoordinate();
         transform.transform(source, target);
 
-        List<String> result = new ArrayList<>();
+        result.add(String.valueOf(target.x));
+        result.add(String.valueOf(target.y));*/
 
-        result.add(String.valueOf(target.x)); //Longitud
-        result.add(String.valueOf(target.y)); //Latitud
-        result.addAll(Utilidades.getGeocodingInfo(target.y, target.x));
+        //result.addAll(Utilidades.getGeocodingInfo());
 
         return result;
     }
